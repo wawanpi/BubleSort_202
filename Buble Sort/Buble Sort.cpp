0380206897536df9c2c073885f2cb85f27cc000a
wawanpi <muhaguskurniawan@gmail.com>
2023-03-17 16:09:33 +0700

    void bubbleSortyArray

--- Buble Sort/Buble Sort.cpp
+++ Buble Sort/Buble Sort.cpp
@@ -39,6 +39,17 @@ void display() {
 	cout << endl;
 }
 
+void bubbleSortArray() {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - i; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
     std::cout << "Hello World!\n";
 }
 
