f7fb244a164992d79f816cb878f67cc461d9ab06
wawanpi <muhaguskurniawan@gmail.com>
2023-03-17 15:51:14 +0700

    Memasukan void

--- Buble Sort/Buble Sort.cpp
+++ Buble Sort/Buble Sort.cpp
@@ -2,8 +2,32 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main()
+int a[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "masukan banyanknya elemen pada array :";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Masukan Element Array" << endl;
+	cout << "===================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ":";
+		cin >> a[i];
+	}
+}
+
 {
     std::cout << "Hello World!\n";
 }
